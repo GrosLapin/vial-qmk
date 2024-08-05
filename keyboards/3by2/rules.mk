1d0ba6aab4266abbffc19bec9acd0b617e732a8c
Martin Morterol <martin.morterol@gmail.com>
2024-08-05 13:15:17 +0200

    stuff 3by2

--- keyboards/3by2/rules.mk
+++ keyboards/3by2/rules.mk
@@ -1,15 +1,7 @@
 # MCU name
-MCU = atmega32u4
+MCU = RP2040
 
-# Bootloader selection
-#   Teensy       halfkay
-#   Pro Micro    caterina
-#   Atmel DFU    atmel-dfu
-#   LUFA DFU     lufa-dfu
-#   QMK DFU      qmk-dfu
-#   ATmega32A    bootloadHID
-#   ATmega328P   USBasp
-BOOTLOADER = caterina
+
 
 EXTRAKEY_ENABLE 	= yes
 NKRO_ENABLE 		= yes
